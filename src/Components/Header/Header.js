a0d860dbbdeb1d7bf6e6c432d1c270a7286b374f
unknown <textile.zero@gmail.com>
2022-10-11 03:30:04 +0600

    Header section added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,43 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Banner from "../../assets/img/banner.jpg"
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
+        <div className="relative">
+            <img
+                src={Banner}
+                className="absolute inset-0 object-cover w-full h-full"
+                alt=""
+            />
+            <div className="relative bg-gray-900 bg-opacity-75 ">
+                <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                    <div className="flex flex-col items-center justify-center xl:flex-row lg:h-[370px]">
+                        <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+                            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                                Winners train losers complain
+                            </h2>
+                            <p className="max-w-xl mb-8  text-white md:text-lg">
+                                Be Ready, No one can stop you.
+                            </p>
+                            <Link
+                                to="/topics"
+                                className='bg-violet-700 hover:bg-violet-900 duration-500 text-white px-8 py-3 rounded'
+                            >
+                                Get Started
+                                <svg
+                                    className="inline-block w-3 ml-2"
+                                    fill="currentColor"
+                                    viewBox="0 0 12 12"
+                                >
+                                    <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                                </svg>
+                            </Link>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,21 +6,23 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
 const Navbar = () => {
     const [open, setOpen] = useState(false);
     return (
-        <div className='bg-white my-2 lg:flex lg:px-8 lg:mx-4 py-4 px-0 justify-between items-center rounded shadow-lg relative'>
-            <div className="logo">
-                <h3 className='text-4xl font-bold italic text-violet-500 mb-3 lg:mb-0'><Link to="/">Quizo</Link></h3>
-            </div>
-            <nav>
-                <div className={`lg:hidden h-8 w-8 absolute right-5 top-8 z-40`} onClick={() => setOpen(!open)}>
-                    {
-                        open ? <XMarkIcon /> : <Bars3Icon />
-                    }
+        <div className='sticky top-0 z-50'>
+            <div className='bg-white lg:flex lg:px-8  py-4 px-0 justify-between items-center rounded shadow-lg relative'>
+                <div className="logo">
+                    <h3 className='text-4xl font-bold italic text-violet-500 mb-3 lg:mb-0'><Link to="/">Quizo</Link></h3>
                 </div>
-                <ul className={`md:py-3 w-full lg:static absolute duration-500 ease-in  lg:bg-white bg-violet-200 ${open ? "top-[65px]" : "top-[350px]"}`}>
-                    <NavItem open={open}></NavItem>
-                </ul>
+                <nav>
+                    <div className={`lg:hidden h-8 w-8 absolute right-5 top-8 z-40`} onClick={() => setOpen(!open)}>
+                        {
+                            open ? <XMarkIcon /> : <Bars3Icon />
+                        }
+                    </div>
+                    <ul className={`md:py-3 w-full lg:static absolute duration-500 ease-in  lg:bg-white bg-violet-200 ${open ? "top-[65px]" : "top-[350px]"}`}>
+                        <NavItem open={open}></NavItem>
+                    </ul>
 
-            </nav>
+                </nav>
+            </div>
         </div>
     );
 };
